e60aa17f36f532c199a967dfa0023e3e41bae147
edbisy <danielyariv21@gmail.com>
2022-08-07 22:14:56 +0300

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '8e1ba972-4396-4d01-bbfb-8d265702529a';
+export const MODAL_CLOSE_SEC = 2.5;
